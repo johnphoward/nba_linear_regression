547f98ca1d84a65d7fe7a9b51012c9759f24f7c6
John Howard <howard.jo@husky.neu.edu>
2017-05-07 11:15:52 -0700

    initial matchup collection code

---
+++ game_lineup_collector.py
@@ -0,0 +1,180 @@
+import json
+import requests
+from matchup import Matchup
+
+
+class DataCollector:
+    PLAYER_1_ID = 13
+    PLAYER_1_TM_ID = 15
+    PLAYER_2_ID = 20
+    PLAYER_2_TM_ID = 22
+    SUB_CODE = 8
+    END_PERIOD_CODE = 13
+
+    def __init__(self):
+        self.nba_stats_base_url = 'http://stats.nba.com/stats/'
+        self.play_by_play_endpoint = 'playbyplayv2'
+        self.lineup_endpoint = 'teamdashlineups'
+        self.base_pbp_params = {
+            'StartPeriod': 1,
+            'EndPeriod': 10,
+        }
+        self.base_lineup_params = {
+            'DateFrom': '',
+            'DateTo': '',
+            'GameSegment': '',
+            'GroupQuantity': 5,
+            'LastNGames': 0,
+            'LeagueID': '00',
+            'Location': '',
+            'MeasureType': 'Base',
+            'Month': '0',
+            'OpponentTeamID': '0',
+            'Outcome': '',
+            'PORound': 0,
+            'PaceAdjust': 'N',
+            'PerMode': 'PerGame',
+            'Period': 0,
+            'PlusMinus': 'N',
+            'Rank': 'N',
+            'Season': '2016-17',
+            'SeasonSegment': '',
+            'SeasonType': 'Regular+Season',
+            'VsConference': '',
+            'VsDivision': '',
+        }
+
+        self.request_headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'User-Agent': 'Mozilla/5.0 Chrome/54.0.2840.71 Safari/537.36',
+            'Referer': 'http://stats.nba.com/game/',
+            'Connection': 'keep-alive'
+        }
+
+    def gather_single_game_lineups(self, game_id):
+        """
+        Given a stats.nba.com game ID number, gather all the lineups from that game
+        Return a list of lineups containing all matchups between the teams and the relevant stats from those matchups
+        """
+        try:
+            # first get the play by play data
+            pbp_headers, pbp_data = self.get_play_by_play_data(game_id)
+
+            p1_index = self.PLAYER_1_TM_ID
+            team_1_id, team_2_id = tuple({play[p1_index] for play in pbp_data if play[p1_index] is not None})
+            end_period_row_numbers = [index for index, row in enumerate(pbp_data) if row[2] == self.END_PERIOD_CODE]
+
+            all_lineups = []
+
+            # loop through each period (quarter/OT) and get the relevant lineups for each
+            for period_index in range(len(end_period_row_numbers)):
+                period_number = period_index + 1
+
+                start_index = 0 if period_index == 0 else end_period_row_numbers[period_index - 1] + 1
+                end_index = end_period_row_numbers[period_index]
+                plays_in_period = pbp_data[start_index: end_index]
+
+                period_lineups = self.parse_out_lineups_for_period(period_number, plays_in_period, team_1_id, team_2_id)
+                all_lineups += period_lineups
+
+            return all_lineups
+
+        except Exception as e:
+            print "Error encountered"
+            print e
+
+    def get_starting_matchups(self, plays, team_1_id, team_2_id, period):
+        """
+        Reads through plays to determine which players started period on the floor.
+        Gets help from stats.nba.com lineup endpoint if necessary.
+        Returns a new Matchup of the ten players that start on the floor
+        """
+        team_1_starters, team_2_starters = [], []
+        substitution_plays = [play for play in plays if play[2] == self.SUB_CODE]
+        subbed_in = set()
+
+        # get id of all players that sub out and add to list if they started the period on the floor
+        for sub_play in substitution_plays:
+            team_list = team_1_starters if sub_play[self.PLAYER_1_TM_ID] == team_1_id else team_2_starters
+
+            out_id = sub_play[self.PLAYER_1_ID]
+            in_id = sub_play[self.PLAYER_2_ID]
+
+            if out_id not in subbed_in:
+                team_list.append(out_id)
+            subbed_in.add(in_id)
+
+        game_id = substitution_plays[0][0]
+
+        # find all
+        if len(team_1_starters) < 5:
+            common_players = self.get_common_players_from_lineups(game_id, team_1_id, period)
+            team_1_starters = team_1_starters + common_players
+        if len(team_2_starters) < 5:
+            common_players = self.get_common_players_from_lineups(game_id, team_2_id, period)
+            team_2_starters = team_2_starters + common_players
+
+        return Matchup(game_id, team_1_id, team_1_starters, team_2_id, team_2_starters)
+
+    def get_common_players_from_lineups(self, game_id, team_id, period):
+        """
+        Get lineups from a given period and find all players who appeared in all of them
+        """
+        lineups = self.get_lineup_data(game_id, team_id, period)
+        player_ids = [set(lineup[1].split(' - ')) for lineup in lineups]
+        return list(map(int, set.intersection(*player_ids)))
+
+    def parse_out_lineups_for_period(self, period, plays_list, first_team_id, second_team_id):
+        """
+        Parse through plays to return all the matchups for a given period
+        """
+        all_matchups = []
+        matchup = self.get_starting_matchups(plays_list, first_team_id, second_team_id, period)
+
+        for play in plays_list:
+            if play[2] == self.SUB_CODE:
+                # add matchup to list and get next one in case of substitution
+                all_matchups.append(matchup)
+                matchup = Matchup.get_new_substitute_lineup(matchup, play)
+            else:
+                # matchup object internally handles play parsing
+                matchup.parse_play_for_stats(play)
+
+        all_matchups.append(matchup)
+
+        return filter(lambda m: m.seconds_played > 0, all_matchups)
+
+    def build_request_url(self, endpoint, params):
+        """
+        Build request for stats.nba.com api given the specific endpoint and parameters
+        return complete URL for GET request
+        """
+        url_base = self.nba_stats_base_url + endpoint + '?'
+        param_string = '&'.join([param_name + '=' + str(param) for param_name, param in params.items()])
+        return url_base + param_string
+
+    def get_play_by_play_data(self, game_id):
+        """
+        Make request to get play by play data and return appropriate data.
+        """
+        parameters = self.base_pbp_params.copy()
+        parameters['GameID'] = game_id
+        url = self.build_request_url(self.play_by_play_endpoint, parameters)
+
+        response = requests.get(url, headers=self.request_headers)
+        data = json.loads(response.content)['resultSets'][0]
+        return data['headers'], data['rowSet']
+
+    def get_lineup_data(self, game_id, team_id, period=0):
+        """
+        Make request to get lineup data and return appropriate data.
+        """
+        parameters = self.base_lineup_params.copy()
+        parameters['GameID'] = game_id
+        parameters['TeamID'] = team_id
+        parameters['Period'] = period
+        url = self.build_request_url(self.lineup_endpoint, parameters)
+
+        response = requests.get(url, headers=self.request_headers)
+        return json.loads(response.content)['resultSets'][1]['rowSet']
+
---
+++ matchup.py
@@ -0,0 +1,200 @@
+from copy import copy
+
+
+class Matchup:
+    PLAYER_1_ID = 13
+    PLAYER_1_TM_ID = 15
+    PLAYER_2_ID = 20
+    PLAYER_2_TM_ID = 22
+
+    PERIOD_START = 12
+    JUMP_BALL = 10
+    MADE_SHOT = 1
+    MISSED_SHOT = 2
+    FT = 3
+    REBOUND = 4
+    TURNOVER = 5
+    FOUL = 6
+    TIMEOUT = 9
+
+    DEFAULT_STATS = {
+        'FGM': 0,
+        'FGA': 0,
+        'FG_PCT': 0,
+        'FG3M': 0,
+        'FG3A': 0,
+        'FG3_PCT': 0,
+        'FTM': 0,
+        'FTA': 0,
+        'FT_PCT': 0,
+        'OREB': 0,
+        'DREB': 0,
+        'REB': 0,
+        'AST': 0,
+        'TOV': 0,
+        'STL': 0,
+        'BLK': 0,
+        'BLKA': 0,
+        'PF': 0,
+        'PFD': 0,
+        'PTS': 0,
+        'PLUS_MINUS': 0,
+    }
+
+    def __init__(self, game_id, team_1_id, team_1_player_ids, team_2_id, team_2_player_ids):
+        self.team_1_id = team_1_id
+        self.team_1_player_ids = team_1_player_ids
+        self.team_1_stats = self.DEFAULT_STATS.copy()
+
+        self.team_2_id = team_2_id
+        self.team_2_player_ids = team_2_player_ids
+        self.team_2_stats = self.DEFAULT_STATS.copy()
+
+        # create an ID for equivalence checks - sort all player IDs and hash result for independence from ordering
+        self.matchup_id = hash(tuple(sorted(self.team_1_player_ids + self.team_2_player_ids)))
+        
+        self.game_ids = [game_id]
+
+        self.games_played = 1
+        self.seconds_played = 0
+        self.minutes_played = 0
+        self.last_timestamp = None
+        self.last_offensive_team = None
+        
+    def has_same_matchups(self, other_matchup):
+        return self.matchup_id == other_matchup.matchup_id
+    
+    def combine_with_same_matchup(self, other_matchup):
+        if not self.has_same_matchups(other_matchup):
+            raise ValueError('Mathups cannot be combined- players do not match')
+        
+        if other_matchup.team_1_id == self.team_1_id:
+            self.team_1_stats = self.combine_stats(self.team_1_stats, other_matchup.team_1_stats)
+            self.team_2_stats = self.combine_stats(self.team_2_stats, other_matchup.team_2_stats)
+        else:
+            self.team_1_stats = self.combine_stats(self.team_1_stats, other_matchup.team_2_stats)
+            self.team_2_stats = self.combine_stats(self.team_2_stats, other_matchup.team_1_stats)
+
+        self.game_ids = list(set(self.game_ids + other_matchup.game_ids))
+        self.games_played = len(self.game_ids)
+        self.seconds_played += other_matchup.seconds_played
+        self.minutes_played = round(self.seconds_played / 60, 1)
+        
+    @staticmethod
+    def combine_stats(stats_a, stats_b):
+        """
+        Given two dictionaries of matchup stats, combine them into one dictionary according to basketball logic
+        """
+        for key, value in stats_b.items():
+            stats_a[key] += value
+
+        stats_a['FG_PCT'] = stats_a['FGM'] * 1.0 / stats_a['FGA']
+        stats_a['FG3_PCT'] = stats_a['FG3M'] / stats_a['FG3A']
+        stats_a['FT_PCT'] = stats_a['FTM'] * 1.0 / stats_a['FTA']
+
+        # TODO: check implementation is correct
+        
+        return stats_a
+
+    @classmethod
+    def get_new_substitute_lineup(cls, matchup, play):
+        """
+        Substitute one player in and return a new Matchup object
+        """
+        team_1_players = copy(matchup.team_1_player_ids)
+        team_2_players = copy(matchup.team_2_player_ids)
+
+        players_to_update = team_1_players if play[Matchup.PLAYER_1_TM_ID] == matchup.team_1_id else team_2_players
+
+        out_id = play[Matchup.PLAYER_1_ID]
+        in_id = play[Matchup.PLAYER_2_ID]
+        player_index = players_to_update.index(out_id)
+        players_to_update[player_index] = in_id
+        
+        game_id = matchup.game_ids[0]
+
+        new_matchup = cls(game_id, matchup.team_1_id, team_1_players, matchup.team_2_id, team_2_players)
+
+        new_matchup.last_timestamp = cls.timestamp_to_seconds(play[6])
+
+        return new_matchup
+
+    @staticmethod
+    def timestamp_to_seconds(timestamp):
+        minutes, seconds = timestamp.split(':')
+        return round(int(minutes) * 60 + float(seconds), 1)
+
+    def parse_play_for_stats(self, play):
+        play_type = play[2]
+        time_of_play = self.timestamp_to_seconds(play[6])
+        primary_team = play[self.PLAYER_1_TM_ID]
+        play_description = play[7] if play[7] is not None else play[9]
+        stats_to_update = self.team_1_stats if primary_team == self.team_1_id else self.team_2_stats
+        other_team_stats = self.team_2_stats if primary_team == self.team_1_id else self.team_1_stats
+
+        if play_type == self.MADE_SHOT:
+            stats_to_update['FGM'] += 1
+            stats_to_update['FGA'] += 1
+            stats_to_update['FG_PCT'] = stats_to_update['FGM'] * 1.0 / stats_to_update['FGA']
+            stats_to_update['PTS'] += 2
+
+            if '3PT' in play_description:
+                stats_to_update['FG3M'] += 1
+                stats_to_update['FG3A'] += 1
+                stats_to_update['FG3_PCT'] = stats_to_update['FG3M'] * 1.0 / stats_to_update['FG3A']
+                stats_to_update['PTS'] += 1
+
+            if play[self.PLAYER_2_TM_ID] is not None:
+                stats_to_update['AST'] += 1
+
+            stats_to_update['PLUS_MINUS'] = stats_to_update['PTS'] - other_team_stats['PTS']
+            other_team_stats['PLUS_MINUS'] = other_team_stats['PTS'] - stats_to_update['PTS']
+        elif play_type == self.MISSED_SHOT:
+            stats_to_update['FGA'] += 1
+            stats_to_update['FG_PCT'] = stats_to_update['FGM'] * 1.0 / stats_to_update['FGA']
+
+            if '3PT' in play_description:
+                stats_to_update['FG3A'] += 1
+                stats_to_update['FG3_PCT'] = stats_to_update['FG3M'] / stats_to_update['FG3A']
+
+            if 'BLK' in play_description:
+                stats_to_update['BLKA'] += 1
+                other_team_stats['BLK'] += 1
+
+            self.last_offensive_team = primary_team
+
+        elif play_type == self.FT:
+            stats_to_update['FTA'] += 1
+
+            if 'MISS' not in play_description:
+                stats_to_update['FTM'] += 1
+                stats_to_update['PTS'] += 1
+                stats_to_update['PLUS_MINUS'] = stats_to_update['PTS'] - other_team_stats['PTS']
+                other_team_stats['PLUS_MINUS'] = other_team_stats['PTS'] - stats_to_update['PTS']
+
+            stats_to_update['FT_PCT'] = stats_to_update['FTM'] * 1.0 / stats_to_update['FTA']
+            self.last_offensive_team = primary_team
+
+        elif play_type == self.REBOUND:
+            stats_to_update['REB'] += 1
+
+            if primary_team == self.last_offensive_team:
+                stats_to_update['OREB'] += 1
+            else:
+                stats_to_update['DREB'] += 1
+
+        elif play_type == self.TURNOVER:
+            stats_to_update['TOV'] += 1
+
+            if play[self.PLAYER_2_TM_ID] is not None:
+                other_team_stats['STL'] += 1
+
+        elif play_type == self.FOUL:
+            stats_to_update['PF'] += 1
+            other_team_stats['PFD'] += 1
+
+        if play_type is not self.PERIOD_START:
+            self.seconds_played += self.last_timestamp - time_of_play
+            self.minutes_played = round(self.seconds_played / 60, 1)
+
+        self.last_timestamp = time_of_play
